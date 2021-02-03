bb3886890fb74642bbebaab6a8d88b86fd4efed0
beggart <b.eggart@gmail.com>
2021-02-03 16:51:54 +0100

    added two functions

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,47 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(m = matrix()) {
   
+  #Initialize
+  a <- NULL
+  
+  # set matrix
+  set <- function(y) {
+    m <<- y
+    a <<- NULL
+  }
+  
+  # get matrix
+  get <- function() {
+    ## Return the matrix
+    m
+  }
+  
+  
+  # set the inverse of the matrix
+  setInverse <- function(inverse) {
+    a <<- inverse
+  }
+  
+  # get the inverse of the matrix
+  getInverse <- function() {
+    ## Return the inverse property
+    a
+  }
+  
+  ## Return the list of the methods
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
+
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
+  
+  
 }
